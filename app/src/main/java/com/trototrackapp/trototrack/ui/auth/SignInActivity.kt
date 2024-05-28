66693a8e68e8432c35a2a03acc19607fe49e7ca8
Renn243 <muhraihanresa243@gmail.com>
2024-05-28 17:46:30 +0800

    feat: add login and signin activity

---
+++ app/src/main/java/com/trototrackapp/trototrack/ui/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.trototrackapp.trototrack.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trototrackapp.trototrack.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
--- app/src/main/java/com/trototrackapp/trototrack/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/welcome/WelcomeActivity.kt
@@ -1,12 +1,27 @@
 package com.trototrackapp.trototrack.ui.welcome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trototrackapp.trototrack.R
+import com.trototrackapp.trototrack.databinding.ActivityWelcomeBinding
+import com.trototrackapp.trototrack.ui.auth.LoginActivity
+import com.trototrackapp.trototrack.ui.auth.SignInActivity
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.LoginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.SignInButton.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
