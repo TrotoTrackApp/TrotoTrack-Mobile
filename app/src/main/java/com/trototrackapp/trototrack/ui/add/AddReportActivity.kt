1bfa40135f218d8e906964281bb58ea16517764b
Muh Raihan Resa <muhraiharesa243@gmail.com>
2024-05-30 13:25:27 +0800

    feat: add addReport page

---
+++ app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
@@ -0,0 +1,12 @@
+package com.trototrackapp.trototrack.ui.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trototrackapp.trototrack.R
+
+class AddReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_report)
+    }
+}
