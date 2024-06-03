e068ef5891b2dddff2041d9ab686c6d319700e3d
Renn243 <muhraihanresa243@gmail.com>
2024-06-04 01:37:41 +0800

    feat: add mapActivity and set the intent for the report flow

--- app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
@@ -1,12 +1,23 @@
 package com.trototrackapp.trototrack.ui.add
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.trototrackapp.trototrack.R
+import com.trototrackapp.trototrack.databinding.ActivityAddReportBinding
+import com.trototrackapp.trototrack.databinding.ActivityLoginBinding
+import com.trototrackapp.trototrack.ui.auth.SignInActivity
 
 class AddReportActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddReportBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_report)
+        binding = ActivityAddReportBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapButton.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/trototrackapp/trototrack/ui/result/ResultActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/result/ResultActivity.kt
@@ -1,12 +1,30 @@
 package com.trototrackapp.trototrack.ui.result
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trototrackapp.trototrack.R
+import com.trototrackapp.trototrack.databinding.ActivityAddReportBinding
+import com.trototrackapp.trototrack.databinding.ActivityResultBinding
+import com.trototrackapp.trototrack.ui.add.AddReportActivity
+import com.trototrackapp.trototrack.ui.add.MapsActivity
+import com.trototrackapp.trototrack.ui.home.MainActivity
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addReportButton.setOnClickListener {
+            startActivity(Intent(this, AddReportActivity::class.java))
+        }
+
+        binding.backButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
