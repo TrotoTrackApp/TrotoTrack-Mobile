fcd0ff95d669cb62eb88d98258fdd08aaa6fe8dc
Renn243 <muhraihanresa243@gmail.com>
2024-06-03 11:46:38 +0800

    feat: add data store for user data and added data checking on splash

--- app/src/main/java/com/trototrackapp/trototrack/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/auth/LoginActivity.kt
@@ -7,11 +7,14 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.trototrackapp.trototrack.data.ResultState
+import com.trototrackapp.trototrack.data.local.UserPreference
 import com.trototrackapp.trototrack.databinding.ActivityLoginBinding
 import com.trototrackapp.trototrack.ui.home.MainActivity
 import com.trototrackapp.trototrack.ui.viewmodel.AuthViewModel
 import com.trototrackapp.trototrack.ui.viewmodel.ViewModelFactory
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,11 +22,14 @@ class LoginActivity : AppCompatActivity() {
     private val authViewModel: AuthViewModel by viewModels {
         ViewModelFactory.getInstance(this)
     }
+    private lateinit var userPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        userPreference = UserPreference.getInstance(this)
 
         binding.LoginButton.setOnClickListener {
             val email = binding.emailEditText.text.toString()
@@ -40,6 +46,20 @@ class LoginActivity : AppCompatActivity() {
                         binding.progressIndicator.visibility = View.GONE
                         Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
                         val intent = Intent(this, MainActivity::class.java)
+
+                        val userData = result.data.data
+                        if (userData != null) {
+                            lifecycleScope.launch {
+                                userPreference.saveData(
+                                    userData.id ?: "",
+                                    userData.name ?: "",
+                                    userData.username ?: "",
+                                    userData.email ?: "",
+                                    userData.token ?: ""
+                                )
+                            }
+                        }
+
                         startActivity(intent)
                         finishAffinity()
                     }
