e8bfdcf967bb341acb2f6b2831d453ef7388f31e
Muh Raihan Resa <muhraiharesa243@gmail.com>
2024-06-05 17:04:02 +0800

    feat: implement api for addReport, edit report and map page ui, edit apiConfig, edit preference

--- app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
@@ -1,23 +1,90 @@
 package com.trototrackapp.trototrack.ui.add
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.trototrackapp.trototrack.data.ResultState
 import com.trototrackapp.trototrack.databinding.ActivityAddReportBinding
-import com.trototrackapp.trototrack.databinding.ActivityLoginBinding
-import com.trototrackapp.trototrack.ui.auth.SignInActivity
+import com.trototrackapp.trototrack.ui.home.MainActivity
+import com.trototrackapp.trototrack.ui.viewmodel.AddReportViewModel
+import com.trototrackapp.trototrack.ui.viewmodel.ViewModelFactory
+import com.trototrackapp.trototrack.util.getImageUri
+import com.trototrackapp.trototrack.util.uriToFile
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.FileOutputStream
 
 class AddReportActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddReportBinding
+    private val addReportViewModel: AddReportViewModel by viewModels {
+        ViewModelFactory.getInstance(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddReportBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val intent = intent
+
+        val isFromMapActivity = intent.getBooleanExtra("FROM_MAP_ACTIVITY", false)
+        if (isFromMapActivity) {
+
+            val locationName = intent.getStringExtra("LOCATION_NAME")
+            val latitude = intent.getDoubleExtra("LATITUDE", 0.0)
+            val longitude = intent.getDoubleExtra("LONGITUDE", 0.0)
+
+            binding.mapLocationEditText.setText(locationName ?: "")
+        }
+
         binding.mapButton.setOnClickListener {
             startActivity(Intent(this, MapsActivity::class.java))
+        }
+
+        binding.submitReportButton.setOnClickListener{
+            val location = binding.mapLocationEditText.text.toString()
+            val reference_location = binding.addressEditText.text.toString()
+            val latitude = intent.getDoubleExtra("LONGITUDE", 0.0)
+            val longitude = intent.getDoubleExtra("LONGITUDE", 0.0)
+            val statusDamage = "heavy damaged"
+            val description = binding.descriptionEditTextLayout.text.toString()
+
+            val imageView = binding.imageHolder
+            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            val file = createTempFile("image", ".jpg")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.close()
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val image: MultipartBody.Part = MultipartBody.Part.createFormData("photo", file.name, requestImageFile)
+
+            binding.progressIndicator.visibility = View.VISIBLE
+            addReportViewModel.addReport(location, reference_location, latitude, longitude, image, statusDamage, description)
+                .observe(this) { result ->
+                    when (result) {
+                        is ResultState.Loading -> {
+                            binding.progressIndicator.visibility = View.VISIBLE
+                        }
+                        is ResultState.Success -> {
+                            binding.progressIndicator.visibility = View.GONE
+                            Toast.makeText(this, "Report added successfully", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finishAffinity()
+                        }
+                        is ResultState.Error -> {
+                            binding.progressIndicator.visibility = View.GONE
+                            Toast.makeText(this, "Error: ${result.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
         }
     }
 }
--- app/src/main/java/com/trototrackapp/trototrack/ui/add/MapsActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/add/MapsActivity.kt
@@ -1,24 +1,33 @@
 package com.trototrackapp.trototrack.ui.add
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.trototrackapp.trototrack.R
 import com.trototrackapp.trototrack.databinding.ActivityMapsBinding
+import java.io.IOException
+import java.util.Locale
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var centerMarker: Marker
+    private lateinit var locationAddressTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +38,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationAddressTextView = findViewById(R.id.locationAddress)
+
+        val buttonDone = findViewById<Button>(R.id.selectLocationButton)
+        buttonDone.setOnClickListener {
+            sendLocationData()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -40,16 +56,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isMapToolbarEnabled = true
 
         getMyLocation()
-    }
 
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                getMyLocation()
+        mMap.setOnMapClickListener { latLng ->
+            if (!::centerMarker.isInitialized) {
+                centerMarker = mMap.addMarker(MarkerOptions().position(latLng).title("Selected Location"))!!
+            } else {
+                centerMarker.position = latLng
             }
+            updateLocationAddress(latLng)
         }
+    }
 
     private fun getMyLocation() {
         if (ContextCompat.checkSelfPermission(
@@ -61,5 +77,43 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         } else {
             requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    private fun updateLocationAddress(latLng: LatLng) {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        try {
+            val addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            if (addresses != null && addresses.isNotEmpty()) {
+                val address = addresses[0].getAddressLine(0)
+                locationAddressTextView.text = address
+            } else {
+                locationAddressTextView.text = "No address found"
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            locationAddressTextView.text = "Unable to get address"
+        }
+    }
+
+    private fun sendLocationData() {
+        val locationName = locationAddressTextView.text.toString()
+        val latLng = centerMarker.position
+
+        val intent = Intent(this, AddReportActivity::class.java).apply {
+            putExtra("LOCATION_NAME", locationName)
+            putExtra("LATITUDE", latLng.latitude)
+            putExtra("LONGITUDE", latLng.longitude)
+            putExtra("FROM_MAP_ACTIVITY", true) // Tambahkan flag untuk menandai bahwa intent ini berasal dari MapActivity
+        }
+        startActivity(intent)
     }
 }
