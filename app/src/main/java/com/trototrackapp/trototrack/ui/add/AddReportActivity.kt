77d14fe30bbd69534748632dbc97448ee3cc9651
Renn243 <muhraihanresa243@gmail.com>
2024-06-06 22:54:00 +0800

    feat: add intent data from resultActivity and fix addReportActivity

--- app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/add/AddReportActivity.kt
@@ -1,13 +1,17 @@
 package com.trototrackapp.trototrack.ui.add
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.trototrackapp.trototrack.data.ResultState
 import com.trototrackapp.trototrack.databinding.ActivityAddReportBinding
 import com.trototrackapp.trototrack.ui.home.MainActivity
@@ -27,25 +31,31 @@ class AddReportActivity : AppCompatActivity() {
         ViewModelFactory.getInstance(this)
     }
 
+    companion object {
+        const val REQUEST_MAP = 1001
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddReportBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = intent
+        val label = intent.getStringExtra("label")
+        val imageUriString = intent.getStringExtra("imageUri")
+
+        Glide.with(this)
+            .load(imageUriString)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(binding.imageHolder)
 
         val isFromMapActivity = intent.getBooleanExtra("FROM_MAP_ACTIVITY", false)
         if (isFromMapActivity) {
-
             val locationName = intent.getStringExtra("LOCATION_NAME")
-            val latitude = intent.getDoubleExtra("LATITUDE", 0.0)
-            val longitude = intent.getDoubleExtra("LONGITUDE", 0.0)
-
             binding.mapLocationEditText.setText(locationName ?: "")
         }
 
         binding.mapButton.setOnClickListener {
-            startActivity(Intent(this, MapsActivity::class.java))
+            startActivityForResult(Intent(this, MapsActivity::class.java), REQUEST_MAP)
         }
 
         binding.submitReportButton.setOnClickListener{
@@ -53,17 +63,15 @@ class AddReportActivity : AppCompatActivity() {
             val reference_location = binding.addressEditText.text.toString()
             val latitude = intent.getDoubleExtra("LONGITUDE", 0.0)
             val longitude = intent.getDoubleExtra("LONGITUDE", 0.0)
-            val statusDamage = "heavy damaged"
+            val statusDamage = label ?: "DefaultStatusDamage"
             val description = binding.descriptionEditTextLayout.text.toString()
 
-            val imageView = binding.imageHolder
-            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-            val file = createTempFile("image", ".jpg")
-            val outputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            outputStream.close()
+            val imageUri = Uri.parse(imageUriString)
+            val file = uriToFile(imageUri, this)
             val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
-            val image: MultipartBody.Part = MultipartBody.Part.createFormData("photo", file.name, requestImageFile)
+            val image: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "image", file.name, requestImageFile
+            )
 
             binding.progressIndicator.visibility = View.VISIBLE
             addReportViewModel.addReport(location, reference_location, latitude, longitude, image, statusDamage, description)
@@ -85,6 +93,16 @@ class AddReportActivity : AppCompatActivity() {
                         }
                     }
                 }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_MAP) {
+            if (resultCode == Activity.RESULT_OK) {
+                val locationName = data?.getStringExtra("LOCATION_NAME")
+                binding.mapLocationEditText.setText(locationName ?: "")
+            }
         }
     }
 }
