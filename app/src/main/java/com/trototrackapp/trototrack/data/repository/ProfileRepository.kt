4f85e478dc3b9a31e2856837b9ee80ef1376e4d5
Muh Raihan Resa <muhraiharesa243@gmail.com>
2024-06-07 20:45:07 +0800

    feat: implement api for UpdateProfile and setup api for getProfile

--- app/src/main/java/com/trototrackapp/trototrack/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/trototrackapp/trototrack/data/repository/ProfileRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.liveData
 import com.trototrackapp.trototrack.data.ResultState
 import com.trototrackapp.trototrack.data.remote.response.UpdateProfileResponse
 import com.trototrackapp.trototrack.data.remote.request.UpdateProfileRequest
+import com.trototrackapp.trototrack.data.remote.response.GetProfileResponse
 import com.trototrackapp.trototrack.data.remote.retrofit.ApiService
 
 class ProfileRepository(private val apiService: ApiService) {
@@ -15,6 +16,21 @@ class ProfileRepository(private val apiService: ApiService) {
             try {
                 val request = UpdateProfileRequest(name, email, username)
                 val response = apiService.updateProfile(request)
+                if (response.isSuccessful) {
+                    emit(ResultState.Success(response.body()!!))
+                } else {
+                    emit(ResultState.Error(response.errorBody()?.string() ?: "An error occurred"))
+                }
+            } catch (e: Exception) {
+                emit(ResultState.Error(e.message ?: "An error occurred"))
+            }
+        }
+
+    fun getUserProfile(): LiveData<ResultState<GetProfileResponse>> =
+        liveData {
+            emit(ResultState.Loading)
+            try {
+                val response = apiService.getUserProfile()
                 if (response.isSuccessful) {
                     emit(ResultState.Success(response.body()!!))
                 } else {
--- app/src/main/java/com/trototrackapp/trototrack/ui/detail/EditProfileActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/detail/EditProfileActivity.kt
@@ -1,18 +1,54 @@
 package com.trototrackapp.trototrack.ui.detail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.trototrackapp.trototrack.data.ResultState
 import com.trototrackapp.trototrack.data.local.UserPreference
 import com.trototrackapp.trototrack.databinding.ActivityEditProfileBinding
+import com.trototrackapp.trototrack.ui.viewmodel.ProfileViewModel
+import com.trototrackapp.trototrack.ui.viewmodel.ViewModelFactory
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
-    private lateinit var userPreference: UserPreference
+    private val profileViewModel: ProfileViewModel by viewModels {
+        ViewModelFactory.getInstance(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.submitButton.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val username = binding.usernameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+
+            binding.progressIndicator.visibility = View.VISIBLE
+            profileViewModel.updateProfile(name, username, email).observe(this) { result ->
+                when (result) {
+                    is ResultState.Loading -> {
+                        binding.progressIndicator.visibility = View.VISIBLE
+                    }
+                    is ResultState.Success -> {
+                        binding.progressIndicator.visibility = View.GONE
+                        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, DetailAccountActivity::class.java)
+
+                        startActivity(intent)
+                        finishAffinity()
+                    }
+                    is ResultState.Error -> {
+                        binding.progressIndicator.visibility = View.GONE
+                        Toast.makeText(this, "Error: ${result.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
