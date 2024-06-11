db117867c5e5c464772761ae40421aa7e0899693
Renn243 <muhraihanresa243@gmail.com>
2024-06-11 23:01:03 +0800

    feat: add new password page

--- app/src/main/java/com/trototrackapp/trototrack/ui/add/NewPasswordActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/add/NewPasswordActivity.kt
@@ -7,34 +7,33 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
-import com.trototrackapp.trototrack.R
 import com.trototrackapp.trototrack.data.ResultState
-import com.trototrackapp.trototrack.databinding.ActivitySendOtpBinding
-import com.trototrackapp.trototrack.databinding.ActivityVerifyOtpBinding
+import com.trototrackapp.trototrack.databinding.ActivityNewPasswordBinding
 import com.trototrackapp.trototrack.ui.home.MainActivity
 import com.trototrackapp.trototrack.ui.viewmodel.ForgetPasswordViewModel
 import com.trototrackapp.trototrack.ui.viewmodel.ViewModelFactory
 import org.json.JSONException
 import org.json.JSONObject
 
-class VerifyOtpActivity : AppCompatActivity() {
+class NewPasswordActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityVerifyOtpBinding
+    private lateinit var binding: ActivityNewPasswordBinding
     private val forgetPasswordViewModel: ForgetPasswordViewModel by viewModels {
         ViewModelFactory.getInstance(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityVerifyOtpBinding.inflate(layoutInflater)
+        binding = ActivityNewPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val email = intent.getStringExtra("email")
 
-        binding.verifyOtpButton.setOnClickListener {
-            val otp = binding.otpEditText.text.toString()
+        binding.submitButton.setOnClickListener {
+            val password = binding.passwordEditText.text.toString()
+            val passwordConfirmation = binding.passwordConfirmationEditText.text.toString()
             if (email != null) {
-                forgetPasswordViewModel.verifyOtp(email, otp).observe(this) { result ->
+                forgetPasswordViewModel.newPassword(email, password, passwordConfirmation).observe(this) { result ->
                     when (result) {
                         is ResultState.Loading -> {
                             binding.progressIndicator.visibility = View.VISIBLE
@@ -43,13 +42,12 @@ class VerifyOtpActivity : AppCompatActivity() {
                         is ResultState.Success -> {
                             binding.progressIndicator.visibility = View.GONE
                             val dialog = AlertDialog.Builder(this)
-                                .setMessage("OTP verified successfully")
+                                .setMessage("New password created successfully")
                                 .setPositiveButton("OK") { dialog, _ ->
                                     dialog.dismiss()
-                                    val intent = Intent(this, NewPasswordActivity::class.java).apply {
-                                        putExtra("email", email)
-                                    }
+                                    val intent = Intent(this, MainActivity::class.java)
                                     startActivity(intent)
+                                    finishAffinity()
                                 }
                                 .create()
                             dialog.show()
