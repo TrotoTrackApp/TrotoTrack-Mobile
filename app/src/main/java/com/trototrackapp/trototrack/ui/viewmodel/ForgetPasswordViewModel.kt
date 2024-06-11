3f183f2cd7251909e6bd614178a7831b959f1e3c
Muh Raihan Resa <muhraiharesa243@gmail.com>
2024-06-11 22:25:14 +0800

    feat: add send otp page

---
+++ app/src/main/java/com/trototrackapp/trototrack/ui/viewmodel/ForgetPasswordViewModel.kt
@@ -0,0 +1,10 @@
+package com.trototrackapp.trototrack.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.trototrackapp.trototrack.data.repository.ForgetPasswordRepository
+
+class ForgetPasswordViewModel(private val forgetPasswordRepository: ForgetPasswordRepository) : ViewModel() {
+
+    fun sendOtp(email: String) = forgetPasswordRepository.sendOtp(email)
+
+}
