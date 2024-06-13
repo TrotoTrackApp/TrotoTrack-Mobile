70304614041df53f7e9d1cb25aa189720c448eeb
Renn243 <muhraihanresa243@gmail.com>
2024-06-13 11:56:59 +0800

    feat: add slider image in home page

--- app/src/main/java/com/trototrackapp/trototrack/ui/home/HomeFragment.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/home/HomeFragment.kt
@@ -2,22 +2,31 @@ package com.trototrackapp.trototrack.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
+import com.trototrackapp.trototrack.R
 import com.trototrackapp.trototrack.data.ResultState
+import com.trototrackapp.trototrack.data.model.SliderData
 import com.trototrackapp.trototrack.databinding.FragmentHomeBinding
 import com.trototrackapp.trototrack.ui.adapter.ArticlesAdapter
+import com.trototrackapp.trototrack.ui.adapter.SliderAdapter
 import com.trototrackapp.trototrack.ui.detail.DetailAccountActivity
 import com.trototrackapp.trototrack.ui.viewmodel.ArticlesViewModel
 import com.trototrackapp.trototrack.ui.viewmodel.ViewModelFactory
 import org.json.JSONException
 import org.json.JSONObject
+import java.text.FieldPosition
 
 class HomeFragment : Fragment() {
 
@@ -27,6 +36,9 @@ class HomeFragment : Fragment() {
     private val articlesViewModel: ArticlesViewModel by viewModels {
         ViewModelFactory.getInstance(requireActivity())
     }
+    private lateinit var slideAdapter: SliderAdapter
+    private val list = ArrayList<SliderData>()
+    private lateinit var dots: ArrayList<TextView>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,9 +54,55 @@ class HomeFragment : Fragment() {
         setupRecyclerView()
         setupObserver()
 
+        list.add(SliderData(R.drawable.home1))
+        list.add(SliderData(R.drawable.app_logo))
+        list.add(SliderData(R.drawable.app_logo))
+
+        slideAdapter = SliderAdapter(list)
+        binding.viewPager.adapter = slideAdapter
+        dots = ArrayList()
+        setIndicator()
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                selectedDot(position)
+                super.onPageSelected(position)
+            }
+
+        })
+
         binding.detailAccountButton.setOnClickListener {
             val intent = Intent(activity, DetailAccountActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun selectedDot(position: Int) {
+        for (i in 0 until list.size) {
+            if (i == position)
+                dots[i].setTextColor(ContextCompat.getColor(requireContext(), R.color.yellow))
+            else
+                dots[i].setTextColor(ContextCompat.getColor(requireContext(), R.color.light_yellow))
+        }
+    }
+
+    private fun setIndicator() {
+        val dotSize = 24
+        val dotMargin = 8
+
+        for (i in 0 until list.size) {
+            dots.add(TextView(requireContext()).apply {
+                text = Html.fromHtml("&#9679;", Html.FROM_HTML_MODE_LEGACY).toString()
+                textSize = dotSize.toFloat()
+                setTextColor(ContextCompat.getColor(requireContext(), R.color.light_yellow))
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                params.setMargins(dotMargin, 0, dotMargin, 0)
+                layoutParams = params
+            })
+            binding.dotsIndicator.addView(dots[i])
         }
     }
 
