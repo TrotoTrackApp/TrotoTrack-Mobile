17fcecda94f2b0e5e0727cd7936fcc45754668b9
Muh Raihan Resa <muhraiharesa243@gmail.com>
2024-06-16 23:39:36 +0800

    feat: implement api to get job data

--- app/src/main/java/com/trototrackapp/trototrack/ui/home/JobFragment.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/home/JobFragment.kt
@@ -6,13 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.trototrackapp.trototrack.data.ResultState
 import com.trototrackapp.trototrack.databinding.FragmentJobBinding
 import com.trototrackapp.trototrack.ui.add.JobActivity
+import com.trototrackapp.trototrack.ui.detail.DetailArticleActivity
+import com.trototrackapp.trototrack.ui.result.JobResultActivity
+import com.trototrackapp.trototrack.ui.viewmodel.JobViewModel
+import com.trototrackapp.trototrack.ui.viewmodel.ViewModelFactory
+import org.json.JSONException
+import org.json.JSONObject
 
 class JobFragment : Fragment() {
 
     private var _binding: FragmentJobBinding? = null
     private val binding get() = _binding!!
+    private val jobViewModel: JobViewModel by viewModels {
+        ViewModelFactory.getInstance(requireActivity())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +38,44 @@ class JobFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupObserver()
+
         binding.applyButton.setOnClickListener {
             val intent = Intent(activity, JobActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setupObserver() {
+        jobViewModel.getJob().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is ResultState.Loading -> {
+
+                }
+                is ResultState.Success -> {
+                    binding.applyButton.setOnClickListener {
+                        val intent = Intent(activity, JobResultActivity::class.java).apply {
+                            putExtra("name", result.data.data?.name)
+                            putExtra("nik", result.data.data?.nik)
+                            putExtra("address", result.data.data?.address)
+                            putExtra("phone", result.data.data?.phone)
+                            putExtra("file", result.data.data?.file)
+                        }
+                        startActivity(intent)
+                    }
+                }
+                is ResultState.Error -> {
+                    val errorMessage = result.message.let {
+                        try {
+                            val json = JSONObject(it)
+                            json.getString("message")
+                        } catch (e: JSONException) {
+                            it
+                        }
+                    } ?: "An error occurred"
+                    Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 }
