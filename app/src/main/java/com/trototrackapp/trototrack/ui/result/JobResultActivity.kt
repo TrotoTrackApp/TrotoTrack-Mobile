fb600dc2bf4fef9067d1ad86adb8f51c83b72506
Renn243 <muhraihanresa243@gmail.com>
2024-06-17 01:55:24 +0800

    feat: job result activity get data job and ui logic

--- app/src/main/java/com/trototrackapp/trototrack/ui/result/JobResultActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/result/JobResultActivity.kt
@@ -1,7 +1,14 @@
 package com.trototrackapp.trototrack.ui.result
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.trototrackapp.trototrack.R
 import com.trototrackapp.trototrack.databinding.ActivityJobResultBinding
 
 class JobResultActivity : AppCompatActivity() {
@@ -12,5 +19,40 @@ class JobResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityJobResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val name = intent.getStringExtra("name")
+        val nik = intent.getStringExtra("nik")
+        val address = intent.getStringExtra("address")
+        val phone = intent.getStringExtra("phone")
+        val status = intent.getStringExtra("status")
+        val fileUrl = intent.getStringExtra("file")
+
+        binding.nameUser.text = name
+        binding.nikUser.text = nik
+        binding.addressUser.text = address
+        binding.phoneUser.text = phone
+
+        val backgroundColor = when (status) {
+            "Pending" -> ContextCompat.getColor(this, R.color.light_yellow)
+            "Rejected" -> ContextCompat.getColor(this, R.color.red)
+            "Approved" -> ContextCompat.getColor(this, R.color.green)
+            else -> Color.TRANSPARENT
+        }
+
+        binding.status.backgroundTintList = ColorStateList.valueOf(backgroundColor)
+
+        binding.downloadButton.setOnClickListener {
+            if (fileUrl != null) {
+                downloadFile(fileUrl)
+            } else {
+                Toast.makeText(this, "File URL is not available", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun downloadFile(fileUrl: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(fileUrl)
+        startActivity(intent)
     }
 }
