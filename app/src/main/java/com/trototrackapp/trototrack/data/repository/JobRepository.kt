8d5806d49c0d75996aae856981adae0d8f87e2cb
Renn243 <muhraihanresa243@gmail.com>
2024-06-16 17:50:15 +0800

    feat: set api service for get data job

--- app/src/main/java/com/trototrackapp/trototrack/data/repository/JobRepository.kt
+++ app/src/main/java/com/trototrackapp/trototrack/data/repository/JobRepository.kt
@@ -3,6 +3,8 @@ package com.trototrackapp.trototrack.data.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import com.trototrackapp.trototrack.data.ResultState
+import com.trototrackapp.trototrack.data.remote.response.GetArticleResponse
+import com.trototrackapp.trototrack.data.remote.response.GetJobResponse
 import com.trototrackapp.trototrack.data.remote.response.JobResponse
 import com.trototrackapp.trototrack.data.remote.retrofit.ApiService
 import okhttp3.MultipartBody
@@ -15,6 +17,21 @@ class JobRepository(private val apiService: ApiService) {
             emit(ResultState.Loading)
             try {
                 val response = apiService.job(name, nik, address, phone, file)
+                if (response.isSuccessful) {
+                    emit(ResultState.Success(response.body()!!))
+                } else {
+                    emit(ResultState.Error(response.errorBody()?.string() ?: "An error occurred"))
+                }
+            } catch (e: Exception) {
+                emit(ResultState.Error(e.message ?: "An error occurred"))
+            }
+        }
+
+    fun getJob(): LiveData<ResultState<GetJobResponse>> =
+        liveData {
+            emit(ResultState.Loading)
+            try {
+                val response = apiService.getJob()
                 if (response.isSuccessful) {
                     emit(ResultState.Success(response.body()!!))
                 } else {
