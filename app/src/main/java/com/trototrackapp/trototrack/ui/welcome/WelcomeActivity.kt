0bc3044b94c0d8c3694193661cc6af83be58e2b5
Renn243 <muhraihanresa243@gmail.com>
2024-06-17 18:39:07 +0800

    feat: add slide animation on welcome page

--- app/src/main/java/com/trototrackapp/trototrack/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/welcome/WelcomeActivity.kt
@@ -3,6 +3,8 @@ package com.trototrackapp.trototrack.ui.welcome
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.trototrackapp.trototrack.R
 import com.trototrackapp.trototrack.databinding.ActivityWelcomeBinding
 import com.trototrackapp.trototrack.ui.auth.LoginActivity
 import com.trototrackapp.trototrack.ui.auth.SignInActivity
@@ -14,6 +16,16 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Animasi Text1 dan View1
+        val animText1 = AnimationUtils.loadAnimation(this, R.anim.slide_right)
+        binding.Text1.startAnimation(animText1)
+        binding.View1.startAnimation(animText1)
+
+        // Animasi Text2 dan View2
+        val animText2 = AnimationUtils.loadAnimation(this, R.anim.slide_left)
+        binding.Text2.startAnimation(animText2)
+        binding.View2.startAnimation(animText2)
 
         binding.LoginButton.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
