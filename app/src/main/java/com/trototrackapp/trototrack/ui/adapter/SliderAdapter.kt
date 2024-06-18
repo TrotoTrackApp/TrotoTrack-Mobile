0bdaa2d7651babc0eacecd9e31e3ffa185629d64
Renn243 <muhraihanresa243@gmail.com>
2024-06-18 20:51:24 +0800

    feat: fix slider bug and edit welcome and job page ui

--- app/src/main/java/com/trototrackapp/trototrack/ui/adapter/SliderAdapter.kt
+++ app/src/main/java/com/trototrackapp/trototrack/ui/adapter/SliderAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.trototrackapp.trototrack.data.model.SliderData
 import com.trototrackapp.trototrack.databinding.ItemSliderBinding
 
@@ -15,6 +16,8 @@ class SliderAdapter(private val items: List<SliderData>) : RecyclerView.Adapter<
             with(binding){
                 Glide.with(itemView)
                     .load(data.imageUrl)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
                     .into(imageView)
             }
         }
